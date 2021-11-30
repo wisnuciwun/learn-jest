e2e88f21f86675b5aa5e95437c356166bad01d33
wisnuciwun <adiwardhanawisnu@gmail.com>
2021-11-30 22:52:39 +0700

    try merge (3)

--- src/index.js
+++ src/index.js
@@ -13,5 +13,5 @@ ReactDOM.hydrate(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitalsss
 reportWebVitals();
